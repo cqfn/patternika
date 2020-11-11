3913d025bcf31b925d6e60f0e824b95c07f4bd82
andrewt0301 <andrewt0301@gmail.com>
2020-11-11 18:42:25 +0300

    Children: fixed PMD error.
    Signed-off-by: andrewt0301 <andrewt0301@gmail.com>

--- src/main/java/org/cqfn/patternika/ast/Children.java
+++ src/main/java/org/cqfn/patternika/ast/Children.java
@@ -11,7 +11,7 @@ import java.util.Objects;
  * <p>Helpful when it is required to iterate over children of a node.
  * Also, provides additional utility methods that perform iteration over children.
  *
- * @param <T> Exact node type, {@link Node} or its subclass.
+ * @param <T> the exact node type, {@link Node} or its subclass.
  *
  * @since 2020/11/2
  */
@@ -22,7 +22,7 @@ public class Children<T extends Node> implements Iterable<T> {
     /**
      * Constructor.
      *
-     * @param parent Parent node with children tp be iterated.
+     * @param parent a parent node with children to be iterated.
      */
     public Children(final T parent) {
         this.parent = Objects.requireNonNull(parent);
@@ -31,7 +31,7 @@ public class Children<T extends Node> implements Iterable<T> {
     /**
      * Returns an iterator over the collection of children.
      *
-     * @return Iterator.
+     * @return iterator.
      */
     @Override
     public Iterator<T> iterator() {
@@ -41,7 +41,7 @@ public class Children<T extends Node> implements Iterable<T> {
     /**
      * Creates and returns a modifiable list of node children.
      *
-     * @return Modifiable list of children.
+     * @return a new modifiable list of children.
      */
     public List<T> toList() {
         final List<T> result = new ArrayList<>(parent.getChildCount());
@@ -50,21 +50,20 @@ public class Children<T extends Node> implements Iterable<T> {
     }
 
     /**
-     * Returns the index of a child in the list of children.
+     * Returns the index of a node in the list of children.
      *
      * <p>NOTE: O(N). Can be optimized to O(1) if it is critical.
      * However, this will require caching iterated children and their indices in a hash map.
      *
-     * @param child Child node.
-     * @return Child index or {@code -1} of there is no such child.
+     * @param node node to be searched among children.
+     * @return a child index or {@code -1} of there is no such node among children.
      */
-    public int indexOf(final Node child) {
-        int index = 0;
-        for (final Node node : this) {
-            if (node == child) {
+    public int indexOf(final Node node) {
+        for (int index = 0; index < parent.getChildCount(); ++index) {
+            final Node child = parent.getChild(index);
+            if (child == node) {
                 return index;
             }
-            ++index;
         }
         return -1;
     }
