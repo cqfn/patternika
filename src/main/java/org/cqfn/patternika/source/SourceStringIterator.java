c1f0d7e2a070f9d4c9070f05715d7d59fda15056
andrewt0301 <andrewt0301@gmail.com>
2020-11-16 19:00:46 +0300

    SourceString is refactored.
    Signed-off-by: andrewt0301 <andrewt0301@gmail.com>

---
+++ src/main/java/org/cqfn/patternika/source/SourceStringIterator.java
@@ -0,0 +1,52 @@
+package org.cqfn.patternika.source;
+
+import java.util.Objects;
+
+/**
+ * Iterator over a string.
+ *
+ * @since 2019/10/28
+ */
+class SourceStringIterator implements SourceIterator {
+    /** Source code represented as a string. */
+    private final String data;
+    /** Current position in source code. */
+    private int index;
+
+    /**
+     * Constructor.
+     *
+     * @param data Source code represented as a string, not {@code null}.
+     */
+    SourceStringIterator(final String data) {
+        this.data = Objects.requireNonNull(data);
+        this.index = 0;
+    }
+
+    @Override
+    public char getChar() {
+        return index < data.length() ? data.charAt(index) : 0;
+    }
+
+    @Override
+    public char getChar(final int offset) {
+        final int pos = index + offset;
+        return pos < data.length() ? data.charAt(pos) : 0;
+    }
+
+    @Override
+    public char nextChar() {
+        return index < data.length() ? data.charAt(index++) : 0;
+    }
+
+    @Override
+    public Position getPosition() {
+        return new SourceStringPosition(index);
+    }
+
+    @Override
+    public Position getPosition(final int offset) {
+        return new SourceStringPosition(index + offset);
+    }
+
+}
---
+++ src/main/java/org/cqfn/patternika/source/SourceStringPosition.java
@@ -0,0 +1,51 @@
+package org.cqfn.patternika.source;
+
+/**
+ * Describes a position in a string.
+ *
+ * @since 2019/10/28
+ */
+class SourceStringPosition implements Position {
+    /** Index of the position in source code. */
+    private final int index;
+
+    /**
+     * Constructor.
+     *
+     * @param index index.
+     */
+    SourceStringPosition(final int index) {
+        this.index = index;
+    }
+
+    @Override
+    public int getIndex() {
+        return index;
+    }
+
+    @Override
+    public int compareTo(final Position other) {
+        checkType(other);
+        return Integer.compare(this.index, other.getIndex());
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(index);
+    }
+
+    /**
+     * Checks that position type is {@link SourceStringPosition}.
+     *
+     * @param position position.
+     * @throws IllegalArgumentException if position is not {@link SourceStringPosition}.
+     */
+    public static void checkType(final Position position) {
+        if (!(position instanceof SourceStringPosition)) {
+            throw new IllegalArgumentException(
+                    "Illegal position type: " + position.getClass().getName()
+            );
+        }
+    }
+
+}
