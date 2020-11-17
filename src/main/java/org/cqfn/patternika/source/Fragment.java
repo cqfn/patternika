98df5873381572fb5bf3639dcf32e9fb2b7f689e
andrewt0301 <andrewt0301@gmail.com>
2020-11-17 10:57:34 +0300

    Fragment is updated.
    Signed-off-by: andrewt0301 <andrewt0301@gmail.com>

--- src/main/java/org/cqfn/patternika/source/Fragment.java
+++ src/main/java/org/cqfn/patternika/source/Fragment.java
@@ -8,11 +8,32 @@ package org.cqfn.patternika.source;
 public interface Fragment {
 
     /**
-     * Returns a source for the given fragment.
+     * Returns a source for the fragment.
      *
      * @return source.
      */
     Source getSource();
 
-}
+    /**
+     * Returns the start position for the fragment.
+     *
+     * @return start position or {@code null} if there is no start.
+     */
+    Position getStart();
 
+    /**
+     * Returns the end position for the fragment.
+     *
+     * @return end position or {@code null} if there is no end.
+     */
+    Position getEnd();
+
+    /**
+     * Checks whether the current fragment contains the other fragment.
+     *
+     * @param other other fragment.
+     * @return {@code true} if this contains other or {@code false} otherwise.
+     */
+    boolean contains(Fragment other);
+
+}
