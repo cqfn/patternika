628e8b71e07da384628969673e3aa201ab99b4c0
andrewt0301 <andrewt0301@gmail.com>
2020-11-19 13:41:36 +0300

    Renaming: Tree -> ActionTree
    Signed-off-by: andrewt0301 <andrewt0301@gmail.com>

--- src/main/java/org/cqfn/patternika/ast/ActionTree.java
+++ src/main/java/org/cqfn/patternika/ast/ActionTree.java
@@ -3,23 +3,23 @@ package org.cqfn.patternika.ast;
 import java.util.Objects;
 
 /**
- * Stores an abstract syntax tree (AST) node and related information.
+ * Stores an abstract syntax tree (AST) with actions and related information.
  *
  * @since 2020/11/9
  */
-public class Tree {
+public class ActionTree {
     /** Language of the code represented by the node tree. */
     private final String language;
-    /** Root of the node tree. */
+    /** Root of the AST tree. */
     private final Node root;
 
     /**
      * Constructor.
      *
-     * @param language Language of the code represented by the node tree.
-     * @param root Root of the node tree.
+     * @param language language of the code represented by the tree, not {@code null}.
+     * @param root root of the AST tree, not {@code null}.
      */
-    public Tree(final String language, final Node root) {
+    public ActionTree(final String language, final Node root) {
         this.language = Objects.requireNonNull(language);
         this.root = Objects.requireNonNull(root);
     }
@@ -27,16 +27,16 @@ public class Tree {
     /**
      * Returns the language of the code represented by the node tree.
      *
-     * @return Language.
+     * @return language.
      */
     public String getLanguage() {
         return language;
     }
 
     /**
-     * Returns the root node of the tree.
+     * Returns the root node of the AST tree.
      *
-     * @return Root node.
+     * @return root node.
      */
     public Node getRoot() {
         return root;
