0c59bf2961f2387fdea269382f1ef159d16ffb56
guseyn <guseynism@gmail.com>
2020-11-19 13:55:05 +0400

    fix checkstyle

--- src/main/java/org/cqfn/patternika/graph/GraphvizUtils.java
+++ src/main/java/org/cqfn/patternika/graph/GraphvizUtils.java
@@ -12,17 +12,23 @@ import java.io.InputStream;
  * Class with set of useful methods for using graphiz app via java.
  * More information about graphviz API you can find here: https://github.com/nidi3/graphviz-java.
  */
-public class GraphvizUtils {
+public final class GraphvizUtils {
+    /**
+     * It's utility class.
+     */
+    private GraphvizUtils() { }
+
     /**
      * Generates graph from .dot file.
      *
      * @param dotFilePath Dot file path for graph generation.
      * @param targetImagePath Target image path for graph.
-     * @throws IOException
+     * @throws IOException if I/O fails.
      */
-    public static void fromDotFile(String dotFilePath, String targetImagePath) throws IOException {
+    public static void fromDotFile(final String dotFilePath,
+                                   final String targetImagePath) throws IOException {
         try (InputStream dot = GraphvizUtils.class.getResourceAsStream(dotFilePath)) {
-            MutableGraph g = new Parser().read(dot);
+            final MutableGraph g = new Parser().read(dot);
             Graphviz.fromGraph(g).render(Format.PNG).toFile(new File(targetImagePath));
         }
     }
@@ -31,10 +37,11 @@ public class GraphvizUtils {
      * Generates graph from .dot content.
      * @param dotContent Dot content for graph generation.
      * @param targetImagePath Target image path for graph.
-     * @throws IOException
+     * @throws IOException if I/O fails.
      */
-    public static void fromDotContent(String dotContent, String targetImagePath) throws IOException {
-        MutableGraph g = new Parser().read(dotContent);
+    public static void fromDotContent(final String dotContent,
+                                      final String targetImagePath) throws IOException {
+        final MutableGraph g = new Parser().read(dotContent);
         Graphviz.fromGraph(g).render(Format.PNG).toFile(new File(targetImagePath));
     }
 }
