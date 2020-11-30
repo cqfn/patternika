4db69953b59d96556cd087d733676aeb528a9d15
andrewt0301 <andrewt0301@gmail.com>
2020-11-30 09:45:33 +0300

    Added packages for Java and XML AST and parser.
    Signed-off-by: andrewt0301 <andrewt0301@gmail.com>

---
+++ src/main/java/org/cqfn/patternika/java/parser/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package contains an classes that implement an Java parser.
+ *
+ * @since 2020/11/30
+ */
+package org.cqfn.patternika.java.parser;
---
+++ src/main/java/org/cqfn/patternika/xml/parser/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package contains an classes that implement an XML parser.
+ *
+ * @since 2020/11/30
+ */
+package org.cqfn.patternika.xml.parser;
