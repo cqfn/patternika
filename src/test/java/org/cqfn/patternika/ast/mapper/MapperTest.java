21bc400d3c6f725e8c009ee78d5b4136b82e1007
andrewt0301 <andrewt0301@gmail.com>
2020-12-17 17:36:14 +0300

    MappedTest is updated.
    Signed-off-by: andrewt0301 <andrewt0301@gmail.com>

--- src/test/java/org/cqfn/patternika/ast/mapper/MapperTest.java
+++ src/test/java/org/cqfn/patternika/ast/mapper/MapperTest.java
@@ -23,13 +23,13 @@ import static org.junit.Assert.assertTrue;
 public class MapperTest {
 
     /**
-     * Tests functionality of {@link GreedMapper}.
+     * Test 1 for functionality of {@link GreedMapper}.
      * <p>
      * Checks that two trees, nodes of which match in the BFS order,
      * are correctly connected in the mapping.
      */
     @Test
-    public void testBfs() {
+    public void testBfs1() {
         final NodeExt root1 = new NodeExt(
             new TestNode("a", 0,
                 new TestNode("a", 1,
@@ -52,16 +52,41 @@ public class MapperTest {
         // Recursively matching subtrees of the two trees must be connected.
         assertMatchesConnected(mapping, root1, root2);
         // Nodes of the two trees traversed in the BFS order must be connected.
-        final Iterator<NodeExt> it1 = new Bfs<>(root1).iterator();
-        final Iterator<NodeExt> it2 = new Bfs<>(root2).iterator();
-        while (it1.hasNext() && it2.hasNext()) {
-            final NodeExt node1 = it1.next();
-            final NodeExt node2 = it2.next();
-            assertSame(mapping.get(node1), node2);
-            assertSame(mapping.get(node2), node1);
-        }
+        assertBfsConnected(mapping, root1, root2);
     }
 
+    /**
+     * Test 2 for functionality of {@link GreedMapper}.
+     * <p>
+     * Checks that two trees, nodes of which match in the BFS order,
+     * are correctly connected in the mapping.
+     */
+    @Test
+    public void testBfs2() {
+        final NodeExt root1 = new NodeExt(
+            new TestNode("a", 0,
+                new TestNode("a", 1,
+                    new TestNode("b", 3,
+                        new TestNode("c", 4)
+                    )
+                ),
+                new TestNode("b", 2)
+            )
+        );
+        final NodeExt root2 = new NodeExt(
+            new TestNode("a", 0,
+                new TestNode("a", 1,
+                    new TestNode("b", 3)
+                ),
+                new TestNode("b", 2)
+            )
+        );
+        final Mapping<NodeExt> mapping = new GreedMapper(root1, root2).buildMapping();
+        // Recursively matching subtrees of the two trees must be connected.
+        assertMatchesConnected(mapping, root1, root2);
+        // Nodes of the two trees traversed in the BFS order must be connected.
+        assertBfsConnected(mapping, root1, root2);
+    }
 
     /**
      * Checks that recursively matching subtrees of the two trees
@@ -72,7 +97,7 @@ public class MapperTest {
      * @param root2 second tree root.
      * @throws AssertionError if at least one pair of matching subtrees is not connected.
      */
-    private void assertMatchesConnected(
+    private static void assertMatchesConnected(
             final Mapping<NodeExt> mapping,
             final NodeExt root1,
             final NodeExt root2) {
@@ -89,6 +114,29 @@ public class MapperTest {
                 }
             }
             assertTrue("No connection for " + source, isConnected);
+        }
+    }
+
+    /**
+     * Checks that two trees, nodes of which match in the BFS order,
+     * are correctly connected in the mapping.
+     *
+     * @param mapping mapping to be checked.
+     * @param root1 first tree root.
+     * @param root2 second tree root.
+     * @throws AssertionError if at least one pair of nodes traversed in BFS order is not connected.
+     */
+    private static void assertBfsConnected(
+            final Mapping<NodeExt> mapping,
+            final NodeExt root1,
+            final NodeExt root2) {
+        final Iterator<NodeExt> it1 = new Bfs<>(root1).iterator();
+        final Iterator<NodeExt> it2 = new Bfs<>(root2).iterator();
+        while (it1.hasNext() && it2.hasNext()) {
+            final NodeExt node1 = it1.next();
+            final NodeExt node2 = it2.next();
+            assertSame(mapping.get(node1), node2);
+            assertSame(mapping.get(node2), node1);
         }
     }
 
