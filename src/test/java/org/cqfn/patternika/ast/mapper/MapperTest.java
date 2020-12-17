6df1162a6f5d2f2a974d6fe4e85a5295a69c1213
andrewt0301 <andrewt0301@gmail.com>
2020-12-17 16:30:44 +0300

    MapperTest is updated.
    Signed-off-by: andrewt0301 <andrewt0301@gmail.com>

--- src/test/java/org/cqfn/patternika/ast/mapper/MapperTest.java
+++ src/test/java/org/cqfn/patternika/ast/mapper/MapperTest.java
@@ -4,26 +4,32 @@ import org.cqfn.patternika.ast.Node;
 import org.cqfn.patternika.ast.NodeExt;
 import org.cqfn.patternika.ast.NodeMatcher;
 import org.cqfn.patternika.ast.TestNode;
+import org.cqfn.patternika.ast.iterator.Bfs;
 
-import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Tests for the {@link AbstractMapper} class.
  *
  * @since 2020/12/17
  */
-public class AbstractMapperTest {
+public class MapperTest {
 
     /**
-     * Test for basic functionality of {@link GreedMapper}.
-     * Checks that recursively matching subtrees of the two trees are connected.
+     * Tests functionality of {@link GreedMapper}.
+     * <p>
+     * Checks that two trees, nodes of which match in the BFS order,
+     * are correctly connected in the mapping.
      */
     @Test
-    public void testMatchingGreedMapper() {
+    public void testBfs() {
         final NodeExt root1 = new NodeExt(
             new TestNode("a", 0,
                 new TestNode("a", 1,
@@ -43,6 +49,33 @@ public class AbstractMapperTest {
             )
         );
         final Mapping<NodeExt> mapping = new GreedMapper(root1, root2).buildMapping();
+        // Recursively matching subtrees of the two trees must be connected.
+        assertMatchesConnected(mapping, root1, root2);
+        // Nodes of the two trees traversed in the BFS order must be connected.
+        final Iterator<NodeExt> it1 = new Bfs<>(root1).iterator();
+        final Iterator<NodeExt> it2 = new Bfs<>(root2).iterator();
+        while (it1.hasNext() && it2.hasNext()) {
+            final NodeExt node1 = it1.next();
+            final NodeExt node2 = it2.next();
+            assertSame(mapping.get(node1), node2);
+            assertSame(mapping.get(node2), node1);
+        }
+    }
+
+
+    /**
+     * Checks that recursively matching subtrees of the two trees
+     * are connected in the mapping.
+     *
+     * @param mapping mapping to be checked.
+     * @param root1 first tree root.
+     * @param root2 second tree root.
+     * @throws AssertionError if at least one pair of matching subtrees is not connected.
+     */
+    private void assertMatchesConnected(
+            final Mapping<NodeExt> mapping,
+            final NodeExt root1,
+            final NodeExt root2) {
         final Map<Node, List<Node>> matches = new NodeMatcher(root1, root2).findAll();
         for (final Map.Entry<Node, List<Node>> entry : matches.entrySet()) {
             final Node source = entry.getKey();
@@ -55,7 +88,7 @@ public class AbstractMapperTest {
                     break;
                 }
             }
-            Assert.assertTrue("No connection for " + source, isConnected);
+            assertTrue("No connection for " + source, isConnected);
         }
     }
 
