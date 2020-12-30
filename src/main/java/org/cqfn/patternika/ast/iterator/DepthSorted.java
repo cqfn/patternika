c4ebcfe2bd2777a146a1eeef4947a317955d5133
andrewt0301 <andrewt0301@gmail.com>
2020-12-30 16:04:29 +0300

    DepthSorted added + relates utility classes.
    Signed-off-by: andrewt0301 <andrewt0301@gmail.com>

---
+++ src/main/java/org/cqfn/patternika/ast/iterator/DepthSorted.java
@@ -0,0 +1,88 @@
+package org.cqfn.patternika.ast.iterator;
+
+import org.cqfn.patternika.ast.Node;
+import org.cqfn.patternika.util.DoubleListIterator;
+import org.cqfn.patternika.util.ReversedListIterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Iterable for node trees, which allows iterating over tree nodes
+ * in the order of decreasing their depths.
+ * <p>
+ * A depth means the maximal distance from the current root node to a leaf node.
+ * <p>
+ * Complexity is O(N), where N is the total number of nodes in the tree.
+ * Sorting by depth is based on a partition sort.
+ *
+ * @param <T> Exact node type, {@link Node} or its subclass.
+ *
+ * @since 2020/12/30
+ */
+public class DepthSorted<T extends Node> implements Iterable<T> {
+    /** List of depths containing nodes having these depths. All depths have at least one node. */
+    private final List<List<T>> nodesByDepth;
+    /** Total count of nodes in the tree. */
+    private int nodeCount;
+
+    /**
+     * Constructor.
+     *
+     * @param root the node tree root.
+     */
+    public DepthSorted(final T root) {
+        this.nodesByDepth = new ArrayList<>();
+        this.nodeCount = 0;
+        addNodes(root);
+    }
+
+    private int addNodes(final T root) {
+        int depth = 0;
+        for (final T child : new Children<>(root)) {
+            depth = Math.max(depth, addNodes(child));
+        }
+        addNode(depth, root);
+        return depth;
+    }
+
+    private void addNode(final int depth, final T node) {
+        final List<T> nodes;
+        if (depth < nodesByDepth.size()) {
+            nodes = nodesByDepth.get(depth);
+        } else {
+            nodes = new ArrayList<>();
+            nodesByDepth.add(depth, nodes);
+        }
+        nodes.add(node);
+        nodeCount++;
+    }
+
+    /**
+     * Returns an iterator over the node tree in the depth-sorted order.
+     *
+     * @return an iterator.
+     */
+    @Override
+    public Iterator<T> iterator() {
+        // We iterate depths from max to min,
+        // for nodes at specific depth we use a default order as it is not important.
+        return new DoubleListIterator<>(
+                new ReversedListIterator<>(nodesByDepth),
+                List::iterator
+            );
+    }
+
+    /**
+     * Creates and returns a modifiable list of depth-sorted nodes.
+     *
+     * @return a new modifiable list of nodes.
+     */
+    public List<T> toList() {
+        final List<T> result = new ArrayList<>(nodeCount);
+        forEach(result::add);
+        return result;
+    }
+
+}
---
+++ src/main/java/org/cqfn/patternika/util/DoubleListIterator.java
@@ -0,0 +1,75 @@
+package org.cqfn.patternika.util;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+import java.util.function.Function;
+
+/**
+ * Iterator that iterates over a double list (a list of lists).
+ *
+ * @param <T> the item type.
+ */
+public final class DoubleListIterator<T> implements Iterator<T> {
+    /** Creates an iterator over items of an item list. */
+    private final Function<List<T>, Iterator<T>> iterFactory;
+    /** Iterator over item lists. */
+    private final Iterator<List<T>> listIter;
+    /** Iterator over items in the current item list. */
+    private Iterator<T> itemIter;
+
+    /**
+     * Default constructor for an iterator, the iterates over
+     * a list in the default order (index increases).
+     *
+     * @param list the list of list to be iterated.
+     */
+    public DoubleListIterator(final List<List<T>> list) {
+        this(list.iterator(), List::iterator);
+    }
+
+    /**
+     * Main constructor.
+     * <p>
+     * Allows creating iterators that iterate in a non-default order (e.g. reverse).
+     *
+     * @param listIter iterator over item lists.
+     * @param iterFactory creates an iterator over items of an item list.
+     */
+    public DoubleListIterator(
+            final Iterator<List<T>> listIter,
+            final Function<List<T>, Iterator<T>> iterFactory) {
+        this.iterFactory = Objects.requireNonNull(iterFactory);
+        this.listIter = Objects.requireNonNull(listIter);
+        this.itemIter = nextItemIter();
+    }
+
+    /**
+     * Returns an iterator over the next item list.
+     *
+     * @return an iterator over the next item list or
+     *         {@code null} if there is no next item list.
+     */
+    private Iterator<T> nextItemIter() {
+        return listIter.hasNext() ? iterFactory.apply(listIter.next()) : null;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return itemIter != null && itemIter.hasNext();
+    }
+
+    @Override
+    public T next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        final T item = itemIter.next();
+        if (!itemIter.hasNext()) {
+            itemIter = nextItemIter();
+        }
+        return item;
+    }
+
+}
