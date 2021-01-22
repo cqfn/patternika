67deb2de081492bc5dcf2737ff5ba4e1700e6957
andrewt0301 <andrewt0301@gmail.com>
2021-01-22 10:43:30 +0300

    Working on JabaParser adapter.
    Signed-off-by: andrewt0301 <andrewt0301@gmail.com>

--- src/main/java/org/cqfn/patternika/lang/java/parser/javaparser/PatternikaJavaParser.java
+++ src/main/java/org/cqfn/patternika/lang/java/parser/javaparser/PatternikaJavaParser.java
@@ -3,12 +3,14 @@ package org.cqfn.patternika.lang.java.parser.javaparser;
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ParseResult;
 import com.github.javaparser.ParseStart;
-
 import com.github.javaparser.Provider;
 import com.github.javaparser.ast.Node;
+
 import org.cqfn.patternika.parser.Parser;
 import org.cqfn.patternika.source.Source;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -21,6 +23,13 @@ import java.util.Optional;
 public class PatternikaJavaParser implements Parser {
     /** Java parser. */
     private final JavaParser parser = new JavaParser();
+    /** Variants of parse starts in the order of likelihood (needed to parse snippets). */
+    private final List<ParseStart<? extends Node>> parseStarts = Arrays.asList(
+            ParseStart.COMPILATION_UNIT,
+            ParseStart.CLASS_OR_INTERFACE_TYPE,
+            ParseStart.CLASS_BODY,
+            ParseStart.BLOCK
+    );
 
     /**
      * Parses the specified source and returns an AST for it.
@@ -30,11 +39,49 @@ public class PatternikaJavaParser implements Parser {
      */
     @Override
     public org.cqfn.patternika.ast.Node parse(final Source source) {
-        final Provider provider = new SourceProvider(source.getIterator());
-        final ParseResult<? extends Node> result =
-                parser.parse(ParseStart.COMPILATION_UNIT, provider);
+        final ParseResult<? extends Node> result = parse(ParseStart.COMPILATION_UNIT, source);
+        if (!result.isSuccessful()) {
+            return null;
+        }
         final Optional<? extends Node> root = result.getResult();
         return convertAst(root.get());
+    }
+
+    /**
+     * Parses a code snippet specified as source and returns an AST for it.
+     *
+     * @param source the source to be parsed.
+     * @return an AST.
+     */
+    @Override
+    public org.cqfn.patternika.ast.Node parseSnippet(final Source source) {
+        ParseResult<? extends Node> result = null;
+        for (final ParseStart<? extends Node> start : parseStarts) {
+            result = parse(start, source);
+            if (result.isSuccessful()) {
+                break;
+            }
+        }
+        if (result == null || !result.isSuccessful()) {
+            return null;
+        }
+        final Optional<? extends Node> root = result.getResult();
+        return convertAst(root.get());
+    }
+
+    /**
+     * Parses the specified source with JavaParser from the specified start.
+     *
+     * @param start specifies what piece of code is to be parsed.
+     * @param source the source to be parsed.
+     * @param <N> the root node type for the constructed AST (depends on start).
+     * @return a parsing result.
+     */
+    protected <N extends Node> ParseResult<N> parse(
+            final ParseStart<N> start,
+            final Source source) {
+        final Provider provider = new SourceProvider(source.getIterator());
+        return parser.parse(start, provider);
     }
 
     private org.cqfn.patternika.ast.Node convertAst(final Node root) {
--- src/main/java/org/cqfn/patternika/parser/Parser.java
+++ src/main/java/org/cqfn/patternika/parser/Parser.java
@@ -4,18 +4,28 @@ import org.cqfn.patternika.ast.Node;
 import org.cqfn.patternika.source.Source;
 
 /**
- * Interface for parsers. Hides implementation details..
+ * Interface for parsers. Hides implementation details.
  *
  * @since 2021/01/21
  */
 public interface Parser {
-
     /**
-     * Parses the specified source and returns an AST for it.
+     * Parses an entire source file specified as source and returns an AST for it.
      *
      * @param source the source to be parsed.
      * @return an AST.
      */
     Node parse(Source source);
 
+    /**
+     * Parses a code snippet specified as source and returns an AST for it.
+     * <p>
+     * A separate method for parsing code snippets is provided because
+     * some parsers are oriented on parsing full source code files and
+     * they require additional actions to parse incomplete code fragments.
+     *
+     * @param source the source to be parsed.
+     * @return an AST.
+     */
+    Node parseSnippet(Source source);
 }
