83cc59a7ad467c26d6f4a1abd92a5941b9d60e46
andrewt0301 <andrewt0301@gmail.com>
2021-01-28 18:35:53 +0300

    JavaParser - refactoring
    Signed-off-by: andrewt0301 <andrewt0301@gmail.com>

--- src/main/java/org/cqfn/patternika/lang/java/parser/javaparser/JavaParserAdapter.java
+++ src/main/java/org/cqfn/patternika/lang/java/parser/javaparser/JavaParserAdapter.java
@@ -1,11 +1,18 @@
 package org.cqfn.patternika.lang.java.parser.javaparser;
 
+import com.github.javaparser.ast.Modifier;
 import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.expr.BinaryExpr;
+import com.github.javaparser.ast.expr.LiteralStringValueExpr;
+import com.github.javaparser.ast.nodeTypes.NodeWithIdentifier;
+import com.github.javaparser.ast.type.PrimitiveType;
 
 import org.cqfn.patternika.parser.Adapter;
+import org.cqfn.patternika.source.Fragment;
 import org.cqfn.patternika.source.Source;
 
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 /**
  * Implementation of {@link Adapter} that adapts JavaParser AST to the Patternika format.
@@ -13,8 +20,6 @@ import java.util.function.Function;
  * @since 2021/01/26
  */
 public class JavaParserAdapter implements Adapter<Node> {
-    /** Function that extracts data from JavaParser nodes depending on their types. */
-    private final Function<Node, String> data = new NodeDataExtractor();
 
     /**
      * Adapts the AST build by JavaParser to the Patternika format.
@@ -25,8 +30,47 @@ public class JavaParserAdapter implements Adapter<Node> {
      */
     @Override
     public JavaNode adapt(final Source source, final Node root) {
-        final FragmentProvider fragments = new FragmentProvider(source);
-        final JavaNodeFactory factory = new JavaNodeFactory(data, fragments);
-        return factory.apply(root);
+        final FragmentProvider fragmentProvider = new FragmentProvider(source);
+        return newJavaNode(root, fragmentProvider);
     }
+
+    /**
+     * Creates a Patternika node for a JavaParser node.
+     *
+     * @param root a JavaParser node.
+     * @param fragmentProvider provides code fragments for JavaParser nodes.
+     * @return a Patternika node.
+     */
+    private JavaNode newJavaNode(final Node root, final FragmentProvider fragmentProvider)  {
+        final String data = getData(root);
+        final Function<Node, JavaNode> factory = node -> newJavaNode(node, fragmentProvider);
+        final Supplier<Fragment> fragment = new FragmentSupplier(root, fragmentProvider);
+        return new JavaNode(root, factory, data, fragment);
+    }
+
+    /**
+     * Gets data from a JaveParser node.
+     *
+     * @param node the JavaParser node.
+     * @return the data from the node or {@code null} if there is not data to return.
+     */
+    private String getData(final Node node) {
+        if (node instanceof Modifier) {
+            return ((Modifier) node).getKeyword().asString();
+        }
+        if (node instanceof NodeWithIdentifier) {
+            return ((NodeWithIdentifier<?>) node).getIdentifier();
+        }
+        if (node instanceof PrimitiveType) {
+            return ((PrimitiveType) node).getType().asString();
+        }
+        if (node instanceof LiteralStringValueExpr) {
+            return ((LiteralStringValueExpr) node).getValue();
+        }
+        if (node instanceof BinaryExpr) {
+            return ((BinaryExpr) node).getOperator().asString();
+        }
+        return null;
+    }
+
 }
