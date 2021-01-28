e89fb1612ed41569cdbd91823d52d5f33ea7fdfb
andrewt0301 <andrewt0301@gmail.com>
2021-01-28 18:14:34 +0300

    JavaParser - draft version
    Signed-off-by: andrewt0301 <andrewt0301@gmail.com>

--- src/main/java/org/cqfn/patternika/lang/java/parser/javaparser/NodeDataExtractor.java
+++ src/main/java/org/cqfn/patternika/lang/java/parser/javaparser/NodeDataExtractor.java
@@ -1,6 +1,11 @@
 package org.cqfn.patternika.lang.java.parser.javaparser;
 
+import com.github.javaparser.ast.Modifier;
 import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.expr.BinaryExpr;
+import com.github.javaparser.ast.expr.LiteralStringValueExpr;
+import com.github.javaparser.ast.nodeTypes.NodeWithIdentifier;
+import com.github.javaparser.ast.type.PrimitiveType;
 
 import java.util.function.Function;
 
@@ -18,6 +23,21 @@ public class NodeDataExtractor implements Function<Node, String> {
      */
     @Override
     public String apply(final Node node) {
+        if (node instanceof Modifier) {
+            return ((Modifier) node).getKeyword().asString();
+        }
+        if (node instanceof NodeWithIdentifier) {
+            return ((NodeWithIdentifier<?>) node).getIdentifier();
+        }
+        if (node instanceof PrimitiveType) {
+            return ((PrimitiveType) node).getType().asString();
+        }
+        if (node instanceof LiteralStringValueExpr) {
+            return ((LiteralStringValueExpr) node).getValue();
+        }
+        if (node instanceof BinaryExpr) {
+            return ((BinaryExpr) node).getOperator().asString();
+        }
         return null;
     }
 }
--- src/test/java/org/cqfn/patternika/lang/java/parser/javaparser/JavaParserAdapterTest.java
+++ src/test/java/org/cqfn/patternika/lang/java/parser/javaparser/JavaParserAdapterTest.java
@@ -1,8 +1,11 @@
 package org.cqfn.patternika.lang.java.parser.javaparser;
 
+import org.cqfn.patternika.ast.Node;
+import org.cqfn.patternika.ast.iterator.Children;
 import org.cqfn.patternika.parser.ParserException;
 import org.cqfn.patternika.source.Source;
 import org.cqfn.patternika.source.SourceFile;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -15,19 +18,21 @@ public class JavaParserAdapterTest {
      */
     private static final String CODE =
           "class X {\n"
+        + "    static { int z = 0; }\n"
         + "    public enum E {\n"
         + "        ONE,\n"
         + "        TWO;\n"
         + "    }\n"
         + "\n"
         + "    private final int x;\n"
+        + "    private final Map<String, List<Integer>> y = new ArrayList<>();\n"
         + "\n"
         + "    public X(int x) {\n"
         + "        this.x = x;\n"
         + "    }\n"
         + "\n"
         + "    public int getX() { return x; }\n"
-        + "    public int add(int y) { return this.x + y + 10; }\n"
+        + "    public int add(@NotNull Integer y) { return this.x + y + 10; }\n"
         + "\n"
         + "    @Override\n"
         + "    public String toString() { return \"X{x=\" + x + '}'; }\n"
@@ -41,8 +46,26 @@ public class JavaParserAdapterTest {
     @Test
     public void test() throws ParserException {
         final Source source = new SourceFile(CODE);
-        final ParserJava parserJava = new ParserJava(new JavaParserAdapter());
-        Assert.assertNotNull(parserJava.parse(source));
+        final ParserJava parser = new ParserJava(new JavaParserAdapter());
+        final Node root = parser.parse(source);
+        Assert.assertNotNull(root);
+        dumpTree(root, 0);
+    }
+
+    @SuppressWarnings("PMD")
+    private void dumpTree(final Node root, final int level) {
+        for (int i = 0; i < level; ++i) {
+            System.out.print("    ");
+        }
+        System.out.print(root.getType());
+        final String data = root.getData();
+        if (data != null) {
+            System.out.print(" : " + root.getData());
+        }
+        System.out.println();
+        for (final Node child : new Children<>(root)) {
+            dumpTree(child, level + 1);
+        }
     }
 
 }
