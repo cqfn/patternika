cac3a8c91ae64ae1aa6af0c811fd64d6906c5ec9
andrewt0301 <andrewt0301@gmail.com>
2021-02-01 09:33:54 +0300

    FragmentProviderTest updated.
    Signed-off-by: andrewt0301 <andrewt0301@gmail.com>

--- src/test/java/org/cqfn/patternika/lang/java/parser/javaparser/FragmentProviderTest.java
+++ src/test/java/org/cqfn/patternika/lang/java/parser/javaparser/FragmentProviderTest.java
@@ -1,7 +1,10 @@
 package org.cqfn.patternika.lang.java.parser.javaparser;
 
+import com.github.javaparser.Range;
+import org.cqfn.patternika.source.Fragment;
 import org.cqfn.patternika.source.SourceFile;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Optional;
@@ -26,7 +29,6 @@ public class FragmentProviderTest {
         + "    return new Fragment(source, start, end);\n"
         + "}";
 
-    /*
     private static Optional<Range> newRange(
             final int beginLine,
             final int beginColumn,
@@ -34,7 +36,15 @@ public class FragmentProviderTest {
             final int endColumn) {
         return Optional.of(Range.range(beginLine, beginColumn, endLine, endColumn));
     }
-    */
+
+    private static Fragment getFragment(
+            final FragmentProvider provider,
+            final int beginLine,
+            final int beginColumn,
+            final int endLine,
+            final int endColumn) {
+        return provider.apply(newRange(beginLine, beginColumn, endLine, endColumn));
+    }
 
     /**
      * Checks that an exception is thrown on empty range.
@@ -44,6 +54,37 @@ public class FragmentProviderTest {
         final SourceFile source = new SourceFile(TEXT);
         final FragmentProvider provider = new FragmentProvider(source);
         provider.apply(Optional.empty());
+    }
+
+    /**
+     * Test that gets a fragment covering the entire source.
+     */
+    @Test
+    public void testAllSource() {
+        final int beginLine = 1;
+        final int beginColumn = 1;
+        final int endLine = 11;
+        final int endColumn = 2;
+        final SourceFile source = new SourceFile(TEXT);
+        final FragmentProvider provider = new FragmentProvider(source);
+        final Fragment fragment = getFragment(provider, beginLine, beginColumn, endLine, endColumn);
+        Assert.assertEquals(TEXT, fragment.toString());
+    }
+
+    /**
+     * Test that takes a fragment of a source and compares it with the original text.
+     */
+    @Test
+    public void test() {
+        final String text = "throw new IllegalArgumentException();";
+        final int beginLine = 5;
+        final int beginColumn = 9;
+        final int endLine = 5;
+        final int endColumn = beginColumn + text.length();
+        final SourceFile source = new SourceFile(TEXT);
+        final FragmentProvider provider = new FragmentProvider(source);
+        final Fragment fragment = getFragment(provider, beginLine, beginColumn, endLine, endColumn);
+        Assert.assertEquals(text, fragment.toString());
     }
 
 }
