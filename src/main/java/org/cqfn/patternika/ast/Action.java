d9ab07e420e8037fd48471baca68425e92b65930
andrewt0301 <andrewt0301@gmail.com>
2021-02-04 18:15:41 +0300

    Action is updated.
    Signed-off-by: andrewt0301 <andrewt0301@gmail.com>

--- src/main/java/org/cqfn/patternika/ast/Action.java
+++ src/main/java/org/cqfn/patternika/ast/Action.java
@@ -5,11 +5,14 @@ import java.util.Objects;
 /**
  * Action that describes a change in an abstract syntax tree.
  *
- * @since 2020/11/6
+ * @since 2020/11/06
  */
+@SuppressWarnings("PMD.DataClass")
 public class Action {
     /** Action type. */
     private final ActionType type;
+    /** Parent node for the reference node (children of this node are updated on action). */
+    private final Node parentNode;
     /** Reference node for the action. */
     private final Node refNode;
     /** Accept node tree for the action. */
@@ -19,14 +22,17 @@ public class Action {
      * Constructor.
      *
      * @param type the action type.
+     * @param parentNode the parent node for the reference node.
      * @param refNode the reference node for the action.
      * @param acceptNode the accept node tree for the action.
      */
     public Action(
             final ActionType type,
+            final Node parentNode,
             final Node refNode,
             final Node acceptNode) {
         this.type = Objects.requireNonNull(type);
+        this.parentNode = Objects.requireNonNull(parentNode);
         this.refNode = refNode;
         this.acceptNode = acceptNode;
     }
@@ -38,6 +44,15 @@ public class Action {
      */
     public ActionType getType() {
         return type;
+    }
+
+    /**
+     * Returns the parent node for the reference node (children of this node are updated on action).
+     *
+     * @return the parent node for the reference node.
+     */
+    public Node getParentNode() {
+        return parentNode;
     }
 
     /**
