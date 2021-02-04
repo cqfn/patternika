985bec2a96aa5a514ed08a36262665c632c4db44
andrewt0301 <andrewt0301@gmail.com>
2021-02-04 17:11:22 +0300

    Action updated and ActionTree added.
    Signed-off-by: andrewt0301 <andrewt0301@gmail.com>

--- src/main/java/org/cqfn/patternika/ast/Action.java
+++ src/main/java/org/cqfn/patternika/ast/Action.java
@@ -1,31 +1,61 @@
 package org.cqfn.patternika.ast;
 
+import java.util.Objects;
+
 /**
- * Action that describes a change in a node tree.
+ * Action that describes a change in an abstract syntax tree.
  *
  * @since 2020/11/6
  */
-public interface Action {
+public class Action {
+    /** Action type. */
+    private final ActionType type;
+    /** Reference node for the action. */
+    private final Node refNode;
+    /** Accept node tree for the action. */
+    private final Node acceptNode;
+
+    /**
+     * Constructor.
+     *
+     * @param type the action type.
+     * @param refNode the reference node for the action.
+     * @param acceptNode the accept node tree for the action.
+     */
+    public Action(
+            final ActionType type,
+            final Node refNode,
+            final Node acceptNode) {
+        this.type = Objects.requireNonNull(type);
+        this.refNode = refNode;
+        this.acceptNode = acceptNode;
+    }
 
     /**
      * Returns identifier that uniquely identifies action type.
      *
      * @return Action type identifier.
      */
-    String getType();
+    public ActionType getType() {
+        return type;
+    }
 
     /**
-     * Returns the reference node for the action (node to be updated).
+     * Returns the reference node for the action (the place where the action is to be applied).
      *
      * @return Reference node.
      */
-    Node getRefNode();
+    public Node getRefNode() {
+        return refNode;
+    }
 
     /**
-     * Returns the accept node tree for the action (new tree to update the reference node).
+     * Returns the accept node tree for the action (new tree to update the tree).
      *
      * @return Accept node tree.
      */
-    Node getAcceptNode();
+    public Node getAcceptNode() {
+        return acceptNode;
+    }
 
 }
---
+++ src/main/java/org/cqfn/patternika/ast/ActionType.java
@@ -0,0 +1,43 @@
+package org.cqfn.patternika.ast;
+
+import java.util.Objects;
+
+/**
+ * Identifiers for action types (types of changes done to an abstract syntax tree).
+ *
+ * @since 2021/02/04
+ */
+public enum ActionType {
+    /** Delete the reference node. */
+    DELETE("ActionDelete"),
+
+    /** Insert a tree before the reference node (as the previous sibling). */
+    INSERT_BEFORE("ActionInsertBefore"),
+
+    /** Insert a tree after  the reference node (as the next sibling). */
+    INSERT_AFTER("ActionInsertAfter"),
+
+    /** Update the data of the reference node. */
+    UPDATE("ActionUpdate");
+
+    /** Printable identifier of the action type. */
+    private final String text;
+
+    /**
+     * Constructor.
+     *
+     * @param text the printable identifier of the action type.
+     */
+    ActionType(final String text) {
+        this.text = Objects.requireNonNull(text);
+    }
+
+    /**
+     * Returns the printable identifier of the action type.
+     *
+     * @return the printable identifier of the action type.
+     */
+    public String getText() {
+        return text;
+    }
+}
