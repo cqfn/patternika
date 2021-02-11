c55c9025b98ac0ff1e8e901c3cd43df040df0cf2
andrewt0301 <andrewt0301@gmail.com>
2021-02-11 14:16:10 +0300

    #34: Hole.getNumber added.
    Signed-off-by: andrewt0301 <andrewt0301@gmail.com>

--- src/main/java/org/cqfn/patternika/ast/Hole.java
+++ src/main/java/org/cqfn/patternika/ast/Hole.java
@@ -6,4 +6,10 @@ package org.cqfn.patternika.ast;
  * @since 2021/02/10
  */
 public interface Hole {
+    /**
+     * Gets the number of the hole that uniquely identifies it.
+     *
+     * @return the number of the hole.
+     */
+    int getNumber();
 }
