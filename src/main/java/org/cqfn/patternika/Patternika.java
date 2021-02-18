a69c5ab93e1cbfd73546a531bb3eebd2712ca5b9
andrewt0301 <andrewt0301@gmail.com>
2021-02-18 15:43:55 +0300

    23#: Entry point (Patternika.java)
    Signed-off-by: andrewt0301 <andrewt0301@gmail.com>

---
+++ src/main/java/org/cqfn/patternika/Patternika.java
@@ -0,0 +1,22 @@
+package org.cqfn.patternika;
+
+/**
+ * The main class (entry point) for the tool.
+ *
+ * @since 2021/02/18
+ */
+public final class Patternika {
+    /**
+     * Disabled constructor: creating instances is forbidden.
+     */
+    private Patternika() { }
+
+    /**
+     * The entry point.
+     *
+     * @param args the command-line arguments.
+     */
+    public static void main(final String[] args) {
+        // Not implemented.
+    }
+}
