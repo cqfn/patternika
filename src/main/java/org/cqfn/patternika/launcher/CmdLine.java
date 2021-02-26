049250e1851d91ac607790cc34930265b611fb28
andrewt0301 <andrewt0301@gmail.com>
2021-02-26 11:20:09 +0300

    #38: CmdLine is updated
    Signed-off-by: andrewt0301 <andrewt0301@gmail.com>

--- src/main/java/org/cqfn/patternika/launcher/CmdLine.java
+++ src/main/java/org/cqfn/patternika/launcher/CmdLine.java
@@ -3,6 +3,7 @@ package org.cqfn.patternika.launcher;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
 
 /**
  * The parsed command line (arguments, options, and relevant action handler).
@@ -16,6 +17,8 @@ public class CmdLine {
     private final Map<String, String> arguments;
     /** Command-line options by names. */
     private final Map<String, List<String>> options;
+    /** Set of ignored option names. */
+    private final Set<String> ignoredOptions;
 
     /**
      * Constructor.
@@ -23,14 +26,17 @@ public class CmdLine {
      * @param handler the handler of the action specified in the command line.
      * @param arguments the command-line argument by names.
      * @param options the command-line options by names.
+     * @param ignoredOptions the set of names of ignored options.
      */
     public CmdLine(
             final Handler handler,
             final Map<String, String> arguments,
-            final Map<String, List<String>> options) {
+            final Map<String, List<String>> options,
+            final Set<String> ignoredOptions) {
         this.handler = Objects.requireNonNull(handler);
         this.arguments = Objects.requireNonNull(arguments);
         this.options = Objects.requireNonNull(options);
+        this.ignoredOptions = Objects.requireNonNull(ignoredOptions);
     }
 
     /**
@@ -68,6 +74,28 @@ public class CmdLine {
      */
     public List<String> getOption(final String name) {
         return options.get(name);
+    }
+
+    /**
+     * Gets the message with information about ignored options.
+     *
+     * @return the message about ignored options.
+     */
+    public String getIgnoredOptions() {
+        if (ignoredOptions.isEmpty()) {
+            return "";
+        }
+        final StringBuilder builder = new StringBuilder("The following options are ignored:");
+        boolean isFirst = true;
+        for (final String option : ignoredOptions) {
+            if (isFirst) {
+                isFirst = false;
+            } else {
+                builder.append(',');
+            }
+            builder.append(" '--").append(option).append('\'');
+        }
+        return builder.toString();
     }
 
 }
