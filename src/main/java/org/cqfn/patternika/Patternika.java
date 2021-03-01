0470340b643eea42baba75cd669bbf783355d2ec
andrewt0301 <andrewt0301@gmail.com>
2021-03-01 16:50:01 +0300

    Patternika is updated.
    Signed-off-by: andrewt0301 <andrewt0301@gmail.com>

--- src/main/java/org/cqfn/patternika/Patternika.java
+++ src/main/java/org/cqfn/patternika/Patternika.java
@@ -1,5 +1,10 @@
 package org.cqfn.patternika;
 
+import org.cqfn.patternika.launcher.CmdLine;
+import org.cqfn.patternika.launcher.CmdLineApi;
+import org.cqfn.patternika.launcher.CmdLineException;
+import org.cqfn.patternika.launcher.CmdLineParser;
+
 /**
  * The main class (entry point) for the tool.
  *
@@ -16,7 +21,27 @@ public final class Patternika {
      *
      * @param args the command-line arguments.
      */
+    @SuppressWarnings("PMD.SystemPrintln")
     public static void main(final String[] args) {
-        // Not implemented.
+        final CmdLineApi api = createApi();
+        final CmdLineParser parser = new CmdLineParser(api);
+        try {
+            final CmdLine cmdLine = parser.parse(args);
+            cmdLine.execute();
+        } catch (final CmdLineException ex) {
+            System.err.print("Invalid command-line format. Error: ");
+            System.err.println(ex.getMessage());
+            System.out.println(api.getReadme());
+        }
     }
+
+    /**
+     * Creates the command-line API for Patternika.
+     *
+     * @return the command-line API for Patternika.
+     */
+    private static CmdLineApi createApi() {
+        return new CmdLineApi();
+    }
+
 }
