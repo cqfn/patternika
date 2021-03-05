31ad9305e22942c2b47ed727efac0b144ca66b79
andrewt0301 <andrewt0301@gmail.com>
2021-03-05 10:15:51 +0300

    Added PatternikaApi
    Signed-off-by: andrewt0301 <andrewt0301@gmail.com>

---
+++ src/main/java/org/cqfn/patternika/handlers/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package contains classes that implements handlers for
+ * actions supported by the Patternika command-line API.
+ *
+ * @since 2021/03/05
+ */
+
+package org.cqfn.patternika.handlers;
