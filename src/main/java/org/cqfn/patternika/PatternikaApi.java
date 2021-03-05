2f4ea63407638a73173876a47b2fc98ca01139bf
andrewt0301 <andrewt0301@gmail.com>
2021-03-05 16:08:59 +0300

    #41: Added sketch handlers
    Signed-off-by: andrewt0301 <andrewt0301@gmail.com>

--- src/main/java/org/cqfn/patternika/PatternikaApi.java
+++ src/main/java/org/cqfn/patternika/PatternikaApi.java
@@ -1,6 +1,11 @@
 package org.cqfn.patternika;
 
+import org.cqfn.patternika.handlers.ParseFileHandler;
+import org.cqfn.patternika.handlers.ParseHandler;
+import org.cqfn.patternika.util.cmdline.Action;
 import org.cqfn.patternika.util.cmdline.CmdLineApi;
+
+import java.util.Collections;
 
 /**
  * Describes the Patternika command-line API.
@@ -9,13 +14,15 @@ import org.cqfn.patternika.util.cmdline.CmdLineApi;
  */
 public class PatternikaApi {
     /** Command-line API. */
-    private final CmdLineApi api;
+    private final CmdLineApi api = new CmdLineApi();
 
     /**
      * Constructor.
      */
     public PatternikaApi() {
-        this.api = new CmdLineApi();
+        registerParse();
+        registerParseFile();
+        registerParseFolder();
     }
 
     /**
@@ -25,6 +32,36 @@ public class PatternikaApi {
      */
     public CmdLineApi getCmdLineApi() {
         return api;
+    }
+
+    private void registerParse() {
+        final Action action = new Action(
+                "parse",
+                "Parse a file or a folder",
+                Collections.singletonList("source"),
+                Collections.emptyList()
+            );
+        api.registerAction(action, new ParseHandler());
+    }
+
+    private void registerParseFile() {
+        final Action action = new Action(
+                "parse-file",
+                "Parse a file",
+                Collections.singletonList("file"),
+                Collections.emptyList()
+            );
+        api.registerAction(action, new ParseFileHandler());
+    }
+
+    private void registerParseFolder() {
+        final Action action = new Action(
+                "parse-folder",
+                "Parse a folder",
+                Collections.singletonList("folder"),
+                Collections.emptyList()
+            );
+        api.registerAction(action, new ParseHandler());
     }
 
 }
---
+++ src/main/java/org/cqfn/patternika/handlers/ParseHandler.java
@@ -0,0 +1,26 @@
+package org.cqfn.patternika.handlers;
+
+import org.cqfn.patternika.util.cmdline.Handler;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Handler for the "parse" command-line action.
+ *
+ * @since 2021/03/05
+ */
+public class ParseHandler implements Handler {
+    /**
+     * Handles a command-line action with specified arguments and options.
+     *
+     * @param arguments the argument by names.
+     * @param options   the options by names (one option can have multiple values).
+     */
+    @Override
+    public void handle(
+            final Map<String, String> arguments,
+            final Map<String, List<String>> options) {
+        // ToDo
+    }
+}
